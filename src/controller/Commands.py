5739add395d069ec4b11efff5c691bac71bafa5e
batuk_t_o <batyuk.to@edu.spbstu.ru>
2026-01-30 00:09:27 +0300

    add class commands.

---
+++ src/controller/Commands.py
@@ -0,0 +1,7 @@
+class Command():
+    def __init__(self, model):
+        self.model = model
+
+    def execute():
+        pass
+
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from controller.Controller import Controller
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
