d859143bf66705e6131d029e9613bc50f58cb1a4
batuk_t_o <batyuk.to@edu.spbstu.ru>
2026-01-31 02:22:54 +0300

    change data.

--- src/model/Data.py
+++ src/model/Data.py
@@ -1,40 +1,49 @@
 from view.View import View
-from .State import State
 from MyString import MyString
 
 
+
 class Data:
-    def __init__(self, view: View):
+    def __init__(self, view: View, state: State):
         # contructor
         self.__posCursor = {'x': 0,
-                            'y': 0
-        }
+                            'y': 0}
         self.__view = view 
         self.__string = MyString()
-        self.__state : list[State] = []
+        self.__state = state
 
     def handleInput(self, commandName):
         self.__state.handleInput(commandName)
-
-    # command with files      
-    def open(self):
-        pass
-    def close(self):
-        pass
-
-    # command with views
-    def update(self):
-        self.__view.draw()
-
-    # command with text
-    def delete(self):
-        pass
-    def cut(self):
-        pass
-    def copy(self):
-        pass
-    def put(self):
-        pass
-
+    
     def posCursor(self, coord, value):
         self.__posCursor[coord]+= value
+
+    def getPosCursor(self):
+        return self.__posCursor
+    
+class Command:
+    def __init__(self, editor: Data):
+        self._editor = editor
+
+    def execute(self):
+        pass
+
+class State:
+    def __init__(self, context : Data):
+        self._commands: dict[Command] = {}
+        self.context = context
+    
+    def handleInput(self, commandName):
+        self._commands[commandName].execute()
+        
+    def addCommmand(self, commandName, command: Command):
+        self._commands[commandName] = command
+        pass
+
+    def ChangeState(self, state: State):
+        pass
+                                                                                                                              
+# view = View()
+# test = Data(view)
+# test.posCursor('x', 10)
+# print(test.getPosCursor())
