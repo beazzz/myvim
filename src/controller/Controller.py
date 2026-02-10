a9d831918f24666deedca96c488f284c533de074
batuk_t_o <batyuk.to@edu.spbstu.ru>
2026-02-10 22:41:37 +0300

    edit view. clear and reflesh

--- src/controller/Controller.py
+++ src/controller/Controller.py
@@ -23,7 +23,9 @@ class ClientController(Controller):
             #     strings.append(command)
 
         posCursor = {'x': 0, 'y':0}
+        self._view.clear()
         self._view.draw(strings, posCursor)
+        self._view.refresh()
         self._view.getkey()
 
     def execute(self):
