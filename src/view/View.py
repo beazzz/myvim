12cd101cb798731e649c3ca877303f7b7f950f26
batuk_t_o <batyuk.to@edu.spbstu.ru>
2026-02-10 03:02:29 +0300

    save

--- src/view/View.py
+++ src/view/View.py
@@ -5,16 +5,19 @@ class View():
     def __init__(self, canvas : curses):
         # print("Create View", self)
         self.__canvas = canvas
+        self.__canvas.scrollok(True)
 
-    def draw(self, strings : list, cursorPos):
+    def draw(self, strings : list[str], cursorPos):
         self.__canvas.clear()
-        # print(string)
-        i = 0
+
+        y = 0
         for string in strings:
-            self.__canvas.addstr(i, 0, string)
-            i += 1
+            y_win, x_win = self.__canvas.getmaxyx()
+            # if (y >= y_win):
+            #     break
+            self.__canvas.addstr(string + '\n')
+            y += 1
 
-
+        self.__canvas.scroll()
         self.__canvas.move(cursorPos['y'], cursorPos['x'])
-            
         self.__canvas.refresh()
