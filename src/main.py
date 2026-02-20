763d158af7342f501b8278d4ab71509f3979f2b2
Тимур <88756957+beazzz@users.noreply.github.com>
2026-02-20 10:21:43 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,23 +3,13 @@ import curses
 
 def main():
     control = ClientController("test.txt")
-    # canvas = curses.initscr()
-    #print(int(canvas.getkey()))
-    # print('\n' == canvas.getkey())
-    # print(canvas.getkey())
-    # print(canvas.getkey())
-    # control.handleInput("\x1b")
     
     done = False
     while not done:
-        # print("draw")
         control.draw()
-        # print("execute")
         control.execute()
 
         done = control.close()
-    
-    # print(str(curses.KEY_UP))
 
 if __name__ == '__main__':
     main()
